3851fae253d4cd19ad4fde959741f113bf63cd07
kmsheng <kmsh3ng@gmail.com>
2019-05-26 21:22:33 +0800

    import styles/application.css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,5 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import '../styles/application.css'
 console.log('Hello World from Webpacker')
