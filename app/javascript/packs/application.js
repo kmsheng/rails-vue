2a1f1a1c7f80219088c8c391020492f4199a3474
kmsheng <kmsh3ng@gmail.com>
2019-05-27 19:57:10 +0800

    remove import application.css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,4 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import '../styles/application.css'
-
 console.log('Hello World from Webpacker')
